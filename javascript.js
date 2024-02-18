4966447482135b33d01132eb6873ffcc247cd798
AlfredoRuiz <alfredoruiz12@outlook.com>
2024-02-18 13:18:49 -0300

    SrAnillos

---
+++ javascript.js
@@ -0,0 +1,39 @@
+//alumnos = prompt("ingrese nombre alumno");
+//document.write("<br>alumnos: "+ alumnos );
+
+//let numero1 = parseInt(prompt("escriba numero 1"));
+//let numero2 = parseInt(prompt("escriba numero 2"));
+
+//if(numero1 >numero2){
+//   document.write("El numero"+numero1+"es masyor perri")
+//}else(document.write("Negativo base el numero"+numero2+"es mayor"))
+
+//let numero1 = parseInt(prompt("escriba numero1"));
+//let numero2 = parseInt(prompt("escriba numero2"));
+//let numero3 = parseInt(prompt("escriba numero3"));
+
+//if(numero1>numero2 && numero1>numero3){
+//  document.write("numero" + numero1+"es mnayor")
+//}else if(numero2 > numero1 && numero2 > numero3){
+//document.write("perri el  numero "+numero2+"es mayor")
+
+//}else{document.write(" no!! el numero "+numero3+"es mayor")}
+
+//let frase = prompt("Escriba una frase");
+
+//console.log(frase);
+//console.log(frase.length);
+//console.log(frase.charAt(1));
+
+//for(let posicion = 0; posicion < frase.length; posicion++ )
+//{
+//    if(frase.charAt(posicion) == "a" || frase.charAt(posicion) == "e" ||frase.charAt(posicion) == "i" || frase.charAt(posicion) == "o" || frase.charAt(posicion) == "u"){
+//        document.write("<br>estas son vocales: " +frase.charAt(posicion));
+//    }
+//}
+
+const myModal = document.getElementById("modalBoro");
+
+function myModal (){
+  document.write("" + myModal)
+}
